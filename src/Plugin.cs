0fefecaaebc245cf42f0935ccc2372038d3c7e7c
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2023-12-06 12:34:33 +0000

    use file-scoped namespace

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -5,45 +5,47 @@ using System.Linq;
 using BepInEx;
 using HarmonyLib;
 
-namespace LethalPosters
-{
-    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    public class Plugin : BaseUnityPlugin
-    {
-        private void Awake()
-        {
-            PosterFolders = Directory.GetDirectories(Paths.PluginPath, PluginInfo.PLUGIN_NAME, SearchOption.AllDirectories).ToList();
-            
-            foreach (var folder in PosterFolders)
-            {
-                foreach (var file in Directory.GetFiles(Path.Combine(folder, "posters")))
-                {
-                    if (Path.GetExtension(file) != ".old")
-                    {
-                        PosterFiles.Add(file);
-                    }
-                }
+namespace LethalPosters;
 
-                foreach (var file in Directory.GetFiles(Path.Combine(folder, "tips")))
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        PosterFolders = Directory.GetDirectories(Paths.PluginPath, PluginInfo.PLUGIN_NAME, SearchOption.AllDirectories).ToList();
+            
+        foreach (var folder in PosterFolders)
+        {
+            foreach (var file in Directory.GetFiles(Path.Combine(folder, "posters")))
+            {
+                if (Path.GetExtension(file) != ".old")
                 {
-                    if (Path.GetExtension(file) != ".old")
-                    {
-                        TipFiles.Add(file);
-                    }
+                    PosterFiles.Add(file);
                 }
             }
 
-            Patches.Init(Logger);
-
-            var harmony = new Harmony(PluginInfo.PLUGIN_GUID);
-            harmony.PatchAll(typeof(Patches));
-            
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_NAME} ({PluginInfo.PLUGIN_VERSION}) is loaded!");
+            foreach (var file in Directory.GetFiles(Path.Combine(folder, "tips")))
+            {
+                if (Path.GetExtension(file) != ".old")
+                {
+                    TipFiles.Add(file);
+                }
+            }
         }
 
-        public static List<string> PosterFolders = new();
-        public static readonly List<string> PosterFiles = new();
-        public static readonly List<string> TipFiles = new();
-        public static Random Rand = new();
+        Patches.Init(Logger);
+
+        var harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+        harmony.PatchAll(typeof(Patches));
+            
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_NAME} ({PluginInfo.PLUGIN_VERSION}) is loaded!");
     }
+        
+        
+        
+
+    public static List<string> PosterFolders = new();
+    public static readonly List<string> PosterFiles = new();
+    public static readonly List<string> TipFiles = new();
+    public static Random Rand = new();
 }
